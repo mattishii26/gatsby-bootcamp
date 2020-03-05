160ea9c503c8785281e54b10fba7d5d18dce8960
mattishii26 <chihiro2657@gmail.com>
2020-03-04 17:53:37 -0800

    Learning Gatsby fast and easy

---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AboutPage = () =>{
+    return (
+        <div>
+            <h1>
+                About Page!
+            </h1>
+            <p>
+                Hello about me!
+            </p>
+        </div>
+    )
+}
+
+export default AboutPage;
---
+++ src/pages/blog.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const BlogPage = () => {
+    return (
+        <div>
+            <h1>
+                Blog
+            </h1>
+            <p>
+                Posts will show up here later on.
+            </p>
+        </div>
+    )
+}
+
+export default BlogPage
